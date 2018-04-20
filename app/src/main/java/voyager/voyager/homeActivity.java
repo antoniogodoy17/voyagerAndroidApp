1fd73a39896954ad96e87dc84e9ecafce7c74089
antoniogodoy17 <antonio.godoy@cetys.edu.mx>
2018-04-20 15:43:11 -0700

    "SDK Changed"

--- app/src/main/java/voyager/voyager/homeActivity.java
+++ app/src/main/java/voyager/voyager/homeActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class homeActivity extends AppCompatActivity {
-    //Testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
