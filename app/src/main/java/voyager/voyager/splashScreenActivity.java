8c251c2e28c984aabcdd43cc6fafeb9fc39820ec
antoniogodoy17 <antonio.godoy@cetys.edu.mx>
2018-04-21 18:09:05 -0700

    Added splash screen

---
+++ app/src/main/java/voyager/voyager/splashScreenActivity.java
@@ -0,0 +1,37 @@
+package voyager.voyager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+
+public class splashScreenActivity extends AppCompatActivity {
+    private LinearLayout l1;
+    Animation alphaAnim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        l1 = findViewById(R.id.l1);
+        alphaAnim = AnimationUtils.loadAnimation(this, R.anim.alpha_transition);
+        l1.setAnimation(alphaAnim);
+
+        final Intent login = new Intent(this,LogInActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(2500);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(login);
+                }
+            }
+        };
+        timer.start();
+    }
+}
