214a5057024200370c093671d3dfc86298df36cf
champagnepollo <guzpref.96@gmail.com>
2018-04-21 21:39:57 -0700

    Sign In Activity

--- app/src/main/java/voyager/voyager/LogInActivity.java
+++ app/src/main/java/voyager/voyager/LogInActivity.java
@@ -1,13 +1,26 @@
 package voyager.voyager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
 
+    Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        btnSignIn = findViewById(R.id.btnSignIn_Login);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signin = new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(signin);
+                finish();
+            }
+        });
     }
 }
