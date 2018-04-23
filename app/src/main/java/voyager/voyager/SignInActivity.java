518681ddcb6e7bc54c86567342f02e85ac375c4d
antoniogodoy17 <tonny-008@hotmail.com>
2018-04-22 18:38:38 -0700

    Date picker dialog added

--- app/src/main/java/voyager/voyager/SignInActivity.java
+++ app/src/main/java/voyager/voyager/SignInActivity.java
@@ -1,21 +1,31 @@
 package voyager.voyager;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SignInActivity extends AppCompatActivity {
 
 
     Button btnLogIn;
     Button btnSignIn;
-    EditText txtname,txtemail,txtpassword,txtbirth_date;
+    EditText txtname,txtemail,txtpassword;
+    TextView txtbirth_date;
     Spinner spnnationality,spnstate,spncity;
-
+    DatePickerDialog datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,15 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         btnLogIn = findViewById(R.id.btnLogIn_SignIn);
         btnSignIn = findViewById(R.id.btnSignIn_Signin);
+        txtname = findViewById(R.id.txtNameSignIn);
+        txtemail = findViewById(R.id.txtEmailSignIn);
+        txtpassword = findViewById(R.id.txtPasswordSignIn);
+        txtbirth_date = findViewById(R.id.txtBirthDateSignIn);
+        spnnationality = findViewById(R.id.sprNationalitySignIn);
+        spnstate = findViewById(R.id.sprEstateSignIn);
+        spncity = findViewById(R.id.sprCitySignIn);
+        btnSignIn = findViewById(R.id.btnSignIn_Signin);
+
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,14 +51,28 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-        txtname = findViewById(R.id.txtNameSignIn);
-        txtemail = findViewById(R.id.txtEmailSignIn);
-        txtpassword = findViewById(R.id.txtPasswordSignIn);
-        txtbirth_date = findViewById(R.id.txtBirthDateSignIn);
-        spnnationality = findViewById(R.id.sprNationalitySignIn);
-        spnstate = findViewById(R.id.sprEstateSignIn);
-        spncity = findViewById(R.id.sprCitySignIn);
-        btnSignIn = findViewById(R.id.btnSignIn_Signin);
+        txtbirth_date.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final Calendar c = Calendar.getInstance();
+                int selYear = c.get(Calendar.YEAR); // current year
+                int selMonth = c.get(Calendar.MONTH); // current month
+                int selDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+                // date picker dialog
+                datePicker = new DatePickerDialog(SignInActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        txtbirth_date.setText(dayOfMonth + "/"+ (monthOfYear + 1) + "/" + year);
+                    }
+                }, selYear, selMonth, selDay);
+                c.add(Calendar.YEAR,-10);
+                datePicker.getDatePicker().setMaxDate(c.getTimeInMillis());
+                c.add(Calendar.YEAR, -100);
+                datePicker.getDatePicker().setMinDate(c.getTimeInMillis());
+                datePicker.show();
+            }
+        });
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
