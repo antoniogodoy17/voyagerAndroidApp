bd5486d8019d60e74bf6df99cf94cd8e9dd22530
Diego <diegocoronado_@outlook.com>
2018-04-29 23:28:24 -0700

    Get activities from db

--- app/src/main/java/voyager/voyager/homeVM.java
+++ app/src/main/java/voyager/voyager/homeVM.java
@@ -1,8 +1,6 @@
 package voyager.voyager;
 
 import android.arch.lifecycle.ViewModel;
-import android.util.Log;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -10,23 +8,42 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import java.util.ArrayList;
 
 public class homeVM extends ViewModel {
     private FirebaseDatabase database;
-    private DatabaseReference usersDatabase;
+    private DatabaseReference usersDatabase,activityDatabase;
     private FirebaseUser fbUser;
     private FirebaseAuth firebaseAuth;
     private User user;
+    private ArrayList<Object> activities;
+    private Actividad activity;
 
     public homeVM(){
         database = FirebaseDatabase.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
         usersDatabase = database.getReference("User");
+        activityDatabase = database.getReference("Actividades");
         fbUser = firebaseAuth.getCurrentUser();
+        activities = new ArrayList<>();
         usersDatabase.orderByChild("email").startAt(fbUser.getEmail()).endAt(fbUser.getEmail() + "\uf8ff").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 user = dataSnapshot.getValue(User.class);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+        activityDatabase.orderByKey().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                activities.add(dataSnapshot.getValue());
             }
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
