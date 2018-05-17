91887eeacc2a8bbe23f3fa9f49f07506a7579523
Jesus Campos <rcr6797@gmail.com>
2018-05-16 20:21:09 -0700

    clean filters

--- app/src/main/java/voyager/voyager/models/FilterInterface/Invoker.java
+++ app/src/main/java/voyager/voyager/models/FilterInterface/Invoker.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import voyager.voyager.models.Activity;
 
 public class Invoker {
-    ArrayList<Activity> filteredActivities = new ArrayList<>();
+    ArrayList<Activity> filteredActivities;
     ArrayList<Filter> filters = new ArrayList();
 
     public void setFilter(Filter filter){
@@ -13,10 +13,18 @@ public class Invoker {
     }
 
     public ArrayList<Activity> applyFilters(){
+        filteredActivities = new ArrayList<>();
         for(int i = 0;i<filters.size();i++){
            filteredActivities = filters.get(i).Execute();
         }
+        cleanFilters();
         return filteredActivities;
+    }
+
+    public void cleanFilters(){
+        for(int i = 0;i<filters.size();i++){
+            filters.remove(i);
+        }
     }
 
 
