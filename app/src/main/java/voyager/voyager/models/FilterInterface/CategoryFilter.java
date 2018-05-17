84f581c83fb03640324991d2e079e47f9cc2e359
Diego <diegocoronado_@outlook.com>
2018-05-16 17:38:29 -0700

    Category and Cost Filter

--- app/src/main/java/voyager/voyager/models/FilterInterface/CategoryFilter.java
+++ app/src/main/java/voyager/voyager/models/FilterInterface/CategoryFilter.java
@@ -8,12 +8,14 @@ public class CategoryFilter implements Filter {
 
     public ArrayList<Activity> Execute(String how, String who, ArrayList<Activity> filterList){
 
+        ArrayList<Activity> newList = new ArrayList<>();
+
         for(int i = 0; i < filterList.size(); i++){
-            if(filterList.get(i).getCategory() != who){
-                filterList.remove(i);
+            if(filterList.get(i).getCategory() == who){
+                newList.add(filterList.get(i));
             }
         }
-        return filterList;
+        return newList;
     }
 }
 
