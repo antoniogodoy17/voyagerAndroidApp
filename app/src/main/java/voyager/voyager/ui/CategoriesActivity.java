c5cc82a8b0c06a9d936b43bddb647e1000ab194c
champagnepollo <guzpref.96@gmail.com>
2018-05-21 23:59:29 -0700

    Categories view updated

--- app/src/main/java/voyager/voyager/ui/CategoriesActivity.java
+++ app/src/main/java/voyager/voyager/ui/CategoriesActivity.java
@@ -1,110 +1,68 @@
 package voyager.voyager.ui;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
+import com.r0adkll.slidr.Slidr;
+import com.r0adkll.slidr.model.SlidrConfig;
+import com.r0adkll.slidr.model.SlidrPosition;
 
 import java.util.ArrayList;
 
-import de.hdodenhof.circleimageview.CircleImageView;
 import voyager.voyager.models.Category;
 import voyager.voyager.R;
 import voyager.voyager.adapters.SliderAdapter;
-import voyager.voyager.models.User;
 
 public class CategoriesActivity extends AppCompatActivity {
     // Database Initialization
     private FirebaseDatabase database;
-    private DatabaseReference userRef, categoryRef;
-    private FirebaseUser fbUser;
-    private FirebaseAuth firebaseAuth;
+    private DatabaseReference categoryRef;
     private FirebaseAuth.AuthStateListener authListener;
     //
     // UI Initialization
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle drawerToggle;
-    View header;
-    TextView drawerUsername;
-    CircleImageView drawerProfilePicture;
     private ViewPager slidePager;
     private LinearLayout dotsLayout;
     private SliderAdapter sliderAdapter;
     private TextView[] dots;
+    private Button closeButton;
     //
     //
     ArrayList<Category> categories;
-    private User user;
     //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        SlidrConfig config = new SlidrConfig.Builder()
+                .position(SlidrPosition.TOP)
+                .build();
+        Slidr.attach(this, config);
 
-        categories = new ArrayList<>();
-        NavigationView navigationView = findViewById(R.id.navigationView);
-        drawerLayout = findViewById(R.id.drawer);
-        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
-        drawerLayout.addDrawerListener(drawerToggle);
-        drawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setupDrawerContent(navigationView);
-
-        header = navigationView.getHeaderView(0);
-        drawerUsername = header.findViewById(R.id.drawerUsername);
-        drawerProfilePicture = header.findViewById(R.id.drawerProfilePicture);
-        header.setOnClickListener(new View.OnClickListener() {
+        closeButton = findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goToProfile();
+                finish();
             }
         });
+
+        categories = new ArrayList<>();
 
         // Database Initialization
         database = FirebaseDatabase.getInstance();
-        firebaseAuth = FirebaseAuth.getInstance();
-        fbUser = firebaseAuth.getCurrentUser();
-        firebaseAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if(fbUser == null){
-                    goToLogin();
-                }
-            }
-        });
-        userRef = database.getReference("User");
-        userRef.child(fbUser.getUid()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                user = dataSnapshot.getValue(User.class);
-                setupDrawerUsername();
-                if(dataSnapshot.hasChild("profile_picture")){
-                    setupDrawerProfilePicture(user.getProfile_picture());
-                }
-//                progressDialog.dismiss();
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {}
-        });
         categoryRef = database.getReference("Category");
         categoryRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -119,12 +77,6 @@ public class CategoriesActivity extends AppCompatActivity {
         });
         // End Database Initialization
     }
-    public void setupDrawerUsername(){
-        drawerUsername.setText(user.getName() + " " + user.getLastname());
-    }
-    public void setupDrawerProfilePicture(String url){
-        Picasso.get().load(url).into(drawerProfilePicture);
-    }
     public void addDotsIndicator(int position){
         dots = new TextView[categories.size()];
         dotsLayout.removeAllViews();
@@ -133,7 +85,7 @@ public class CategoriesActivity extends AppCompatActivity {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226"));
             dots[i].setTextSize(35);
-            dots[i].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+            dots[i].setTextColor(getResources().getColor(R.color.colorSecondary));
             dotsLayout.addView(dots[i]);
         }
         if(dots.length > 0){
@@ -150,69 +102,6 @@ public class CategoriesActivity extends AppCompatActivity {
         @Override
         public void onPageScrollStateChanged(int state) {}
     };
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(drawerToggle.onOptionsItemSelected(item)){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    private void setupDrawerContent(NavigationView navigationView){
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                selectDrawerMenu(item);
-                return true;
-            }
-        });
-    }
-
-    public void selectDrawerMenu(MenuItem menu){
-        Class intentClass = null;
-        switch (menu.getItemId()){
-            case R.id.homeMenu:
-                intentClass = HomeActivity.class;
-                break;
-            case R.id.categoriesMenu:
-                intentClass = CategoriesActivity.class;
-                break;
-            case R.id.favoritesMenu:
-                intentClass = ListActivity.class;
-                break;
-            case R.id.listsMenu:
-                intentClass = ListsActivity.class;
-                break;
-            case R.id.switchLocationMenu:
-                intentClass = SwitchLocationActivity.class;
-                break;
-            case R.id.nearMeMenu:
-                intentClass = MapsActivity.class;
-                break;
-            case R.id.logoutMenu:
-                intentClass = LogInActivity.class;
-                firebaseAuth.signOut();
-                break;
-        }
-        if(intentClass != this.getClass() && intentClass != null){
-            Intent nextView = new Intent(this,intentClass);
-            if(intentClass == ListActivity.class){
-                nextView.putExtra("list","favorites");
-            }
-            startActivity(nextView);
-            drawerLayout.closeDrawers();
-        }
-    }
-    public void goToProfile(){
-        Intent profile = new Intent(this,ProfileActivity.class);
-        startActivity(profile);
-        finish();
-    }
-    public void goToLogin(){
-        Intent login = new Intent(this,LogInActivity.class);
-        login.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(login);
-        finish();
-    }
 
     protected void fillCategories(DataSnapshot data){
         for(DataSnapshot ds:data.getChildren()){
